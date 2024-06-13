a50c84afa2e8838f63bef5a9c68dd03063b9db7d
Ibad <ibad.src@gmail.com>
2024-06-13 16:55:25 +0500

    fixed custom datatype

--- Components/Graph.tsx
+++ Components/Graph.tsx
@@ -9,7 +9,7 @@ const { width, height } = Dimensions.get('window');
   interface GraphProps {
     data: GraphData;
     chartType: 'line' | 'bar';
-    dataType: 'monthly' | 'weekly' | 'daily';
+    dataType: 'monthly' | 'weekly' | 'daily' | 'custom';
   }
 
   interface DataPointClickEvent {
